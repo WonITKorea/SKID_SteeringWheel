f4be0f2accc53dcc6312daf76aa7965bdbefcc09
Chae Won Lim <75358803+WonITKorea@users.noreply.github.com>
2023-11-09 15:29:50 +0900

    remove Main LED Code

--- Main.py
+++ Main.py
@@ -20,21 +20,6 @@ machine
 # Timer
 timer = machine.Timer()
 
-# Configure the RPM Indication LEDs(WS2812B)
-NUM_LEDS = 25
-PIN_NUM = 22 # Data Pin Num.
-brightness = 0.2 # Between 0~1, Higher the value, Brighter the LEDs.
-
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
-GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
-BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
-WHITE = (255, 255, 255)
-COLORS = (BLACK, RED, YELLOW, GREEN, CYAN, BLUE, PURPLE, WHITE)
-
 #Temp Sensor Initialization
 OBTemp = OBTRead()
 DHTemp = DHTRead()
