72b8369a1493a32e6977c2b4d58ebb0dd69d8e0d
Chae Won Lim <75358803+WonITKorea@users.noreply.github.com>
2023-11-09 15:25:20 +0900

    Add RGB LED Library code

---
+++ WS2812BC.py
@@ -0,0 +1,12 @@
+import neopixel
+
+# 32 LED strip connected to X8.
+p = machine.Pin.board.X8
+n = neopixel.NeoPixel(p, 32)
+
+# Draw a red gradient.
+for i in range(32):
+    n[i] = (i * 8, 0, 0)
+
+# Update the strip.
+n.write()
